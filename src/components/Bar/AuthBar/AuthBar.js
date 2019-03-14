2caa6af9b2fe76de7bed2eca696ef0c4b8d620b0
Your Name <wamontgomery0068@gmail.com>
2019-03-14 09:03:57 -0600

    Initial Set-Up

---
+++ src/components/Bar/AuthBar/AuthBar.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+
+import './AuthBar.css';
+
+class AuthBar extends Component {
+    render(){
+        return (
+            <div className = "AuthBar_Container">
+                <h1> Authorization Bar </h1>
+            </div>
+        )
+    }
+}
+
+export default AuthBar;
