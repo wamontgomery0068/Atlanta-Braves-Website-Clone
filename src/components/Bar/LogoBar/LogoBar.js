d5abcb471add6c5fafaebcaee77af19343d841ee
Your Name <wamontgomery0068@gmail.com>
2019-03-14 13:10:56 -0600

    Logos Placed

--- src/components/Bar/LogoBar/LogoBar.js
+++ src/components/Bar/LogoBar/LogoBar.js
@@ -6,7 +6,8 @@ class LogoBar extends Component {
     render(){
         return (
             <div className = "LogoBar_Container">
-                <h3> Logo Bar </h3>
+                <img className = "Tomahawk_Logo" src = "http://www.mlbstatic.com/mlb.com/builds/site-core/f457317af052a355307a55ec74e6730331d8d9dc_1552504434/images/logos/team-primary-on-dark/144.svg" atl = "Tomahawk"/>
+                <img className = "Braves_Logo" src = "https://content.mlb.com/documents/9/0/4/266006904/ATL_url.svg" atl = "Braves.com"/>
             </div>
         )
     }
