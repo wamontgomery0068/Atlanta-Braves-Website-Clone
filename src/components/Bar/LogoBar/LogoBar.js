a5dfd50b86ef2f7393a7091bc30b8624740e145c
Your Name <wamontgomery0068@gmail.com>
2019-03-14 09:06:48 -0600

    Initial Set-up

---
+++ src/components/Bar/LogoBar/LogoBar.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+
+import './LogoBar.css';
+
+class LogoBar extends Component {
+    render(){
+        return (
+            <div className = "LogoBar_Container">
+                <h1> Team Logo Bar </h1>
+            </div>
+        )
+    }
+}
+
+export default LogoBar;
