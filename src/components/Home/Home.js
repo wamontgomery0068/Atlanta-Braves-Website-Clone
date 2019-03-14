7fc17ae97a4364aabd602a8794799ebc8c31176f
Your Name <wamontgomery0068@gmail.com>
2019-03-14 08:39:26 -0600

    Initial Set Up

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+
+import './Home.css';
+
+class Home extends Component {
+    render(){
+        return (
+            <div className = "Home_Container">
+                <h1> Home Page </h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
