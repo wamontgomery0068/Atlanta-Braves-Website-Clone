14f4ac408b66ae43bf525b6123ee6f6a81078baf
Your Name <wamontgomery0068@gmail.com>
2019-03-14 09:04:39 -0600

    Import the AuthBar Component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React, { Component} from 'react';
 
-import NavBar from '../NavBar/NavBar';
+import AuthBar from '../Bar/AuthBar/AuthBar';
+import NavBar from '../Bar/NavBar/NavBar';
 import './Home.css';
 
 class Home extends Component {
     render(){
         return (
             <div className = "Home_Container">
+                <AuthBar />
                 <NavBar />
                 <h1> Home Page </h1>
             </div>
