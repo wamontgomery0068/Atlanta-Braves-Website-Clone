b9427c60fd174c92391a2b2c57406507ab4813e2
Your Name <wamontgomery0068@gmail.com>
2019-03-14 08:53:38 -0600

    Initial Set Up

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+
+import './NavBar.css';
+
+class NavBar extends Component {
+    render(){
+        return (
+            <div className = "NavBar_Container">
+                <h1> Navigation Bar </h1>
+            </div>
+        )
+    }
+}
+
+export default NavBar;
