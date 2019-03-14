36a021aa975bd9dd9143b3883ebc5e497cb0ffce
Your Name <wamontgomery0068@gmail.com>
2019-03-14 11:41:26 -0600

    Routes are set up

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { Switch, Route } from "react-router-dom";
 
 import Home from './components/Home/Home';
-
+import News from './components/News/News';
+import Scores from './components/Scores/Scores';
+import Roster from './components/Roster/Roster';
+import Schedule from './components/Schedule/Schedule';
 
 
 
 export default (
     <Switch>
         <Route path = "/" exact component = {Home} />
+        <Route path = "/news" component = {News} />
+        <Route path = "/scores" component = {Scores} />
+        <Route path = "/roster" component = {Roster} />
+        <Route path = "/schedule" component = {Schedule} />
     </Switch>
 );
