05a8de46153005777b731d363f40b2ebdee4737d
Your Name <wamontgomery0068@gmail.com>
2019-03-14 08:40:19 -0600

    Imported BrowserRouter and the Routes file.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import routes from "./routes";
+
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
-    );
+      <Router>
+        <div className = "App_Container">
+          {routes}
+        </div>
+      </Router>
+    )
   }
 }
 
