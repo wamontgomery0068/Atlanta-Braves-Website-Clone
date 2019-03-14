28857e839c8e35f7a82959f307dd9e49a7afbb1a
Your Name <wamontgomery0068@gmail.com>
2019-03-14 15:49:33 -0600

    Made Tomahawk Logo Link to Home Page

--- src/components/Bar/LogoBar/LogoBar.js
+++ src/components/Bar/LogoBar/LogoBar.js
@@ -1,4 +1,5 @@
 import React, { Component} from 'react';
+import { Link } from 'react-router-dom';
 
 import './LogoBar.css';
 
@@ -6,7 +7,9 @@ class LogoBar extends Component {
     render(){
         return (
             <div className = "LogoBar_Container">
-                <img className = "Tomahawk_Logo" alt="" src = "http://www.mlbstatic.com/mlb.com/builds/site-core/f457317af052a355307a55ec74e6730331d8d9dc_1552504434/images/logos/team-primary-on-dark/144.svg" />
+                <Link to = "/">
+                    <img className = "Tomahawk_Logo" alt="" src = "http://www.mlbstatic.com/mlb.com/builds/site-core/f457317af052a355307a55ec74e6730331d8d9dc_1552504434/images/logos/team-primary-on-dark/144.svg" title = "Home" />
+                </Link>
                 <img className = "Braves_Logo" alt="" src = "https://content.mlb.com/documents/9/0/4/266006904/ATL_url.svg" />
             </div>
         )
