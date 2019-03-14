abd6c74609e8321205bd2a752fbd38575c54444d
Your Name <wamontgomery0068@gmail.com>
2019-03-14 08:54:16 -0600

    Imported the NavBar Component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { Component} from 'react';
 
+import NavBar from '../NavBar/NavBar';
 import './Home.css';
 
 class Home extends Component {
     render(){
         return (
             <div className = "Home_Container">
+                <NavBar />
                 <h1> Home Page </h1>
             </div>
         )
