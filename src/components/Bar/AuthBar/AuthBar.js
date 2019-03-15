dea809efc62a1079a87a53863ec8a7cd4681db3a
Your Name <wamontgomery0068@gmail.com>
2019-03-15 09:29:24 -0600

    Changed Offical to Unoffical

--- src/components/Bar/AuthBar/AuthBar.js
+++ src/components/Bar/AuthBar/AuthBar.js
@@ -7,7 +7,7 @@ class AuthBar extends Component {
         return (
             <div className = "AuthBar_Container">
                 <img className = "MLB_Logo" src = "https://www.mlbstatic.com/team-logos/league-on-dark/1.svg" alt="MLB_Logo" />
-                <p className = "Offical_Text"> The Offical Site of the Atlanta Braves </p> 
+                <p className = "Offical_Text"> The Unoffical Site of the Atlanta Braves </p> 
                 <img className = "Blank_Profile_Logo" alt="" src = "https://secure.ui.bamstatic.com/bridge/images/headers/default_avatar.png"/>
             </div>
         )
