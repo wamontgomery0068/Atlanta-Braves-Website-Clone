792d2240fdc9e3e1bfcd10a2a11219b85cb2d084
Your Name <wamontgomery0068@gmail.com>
2019-03-15 09:26:57 -0600

    Linked Nav Text List. Links to pages WORK!

--- src/components/Bar/NavBar/NavBar.js
+++ src/components/Bar/NavBar/NavBar.js
@@ -1,5 +1,7 @@
 import React, { Component} from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+
 
 import './NavBar.css';
 
@@ -7,10 +9,18 @@ class NavBar extends Component {
     render(){
         return (
             <div className = "NavBar_Container">
-                <p className = "Link_Text"> News </p>
-                <p className = "Link_Text"> Scores </p>
-                <p className = "Link_Text"> Schedule </p>
-                <p className = "Link_Text"> Roster </p>
+                <Link className = "Link_Properties" to = "/news">
+                    <p className = "Link_Text"> News </p>
+                </Link>
+                <Link className = "Link_Properties" to = "scores">
+                    <p className = "Link_Text"> Scores </p>
+                </Link>
+                <Link className = "Link_Properties" to = "/schedule">
+                    <p className = "Link_Text"> Schedule </p>
+                </Link>
+                <Link className = "Link_Properties" to = "/roster">
+                    <p className = "Link_Text"> Roster </p>
+                </Link>
             </div>
         )
     }
