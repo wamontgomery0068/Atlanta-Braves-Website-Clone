176d2bbc7ab080d46528e271d1721d6e0dd68dae
Your Name <wamontgomery0068@gmail.com>
2019-03-18 09:55:55 -0600

    Building the DropDown

--- src/components/Bar/NavBar/NavBar.js
+++ src/components/Bar/NavBar/NavBar.js
@@ -9,21 +9,33 @@ class NavBar extends Component {
     render(){
         return (
             <div className = "NavBar_Container">
-                <Link className = "Link_Properties" to = "/news">
-                    <p className = "Link_Text"> News </p>
-                </Link>
-                <Link className = "Link_Properties" to = "scores">
-                    <p className = "Link_Text"> Scores </p>
-                </Link>
-                <Link className = "Link_Properties" to = "/schedule">
-                    <p className = "Link_Text"> Schedule </p>
-                </Link>
-                <Link className = "Link_Properties" to = "/roster">
-                    <p className = "Link_Text"> Roster </p>
-                </Link>
+                <div className = "Nav_List_Container">
+                    <button className = "Nav_DropDown_Button"> News </button>
+                    <button className = "Nav_DropDown_Button"> Scores </button>
+                    <button className = "Nav_DropDown_Button"> Schedule </button>
+                    <button className = "Nav_DropDown_Button"> Roster </button>
+                </div>
+                <div className = "Nav_DropDown_Container">
+                    <div className = "Nav_DropDown"></div>
+                    <div className = "Nav_DropDown"></div>
+                    <div className = "Nav_DropDown"></div>
+                    <div className = "Nav_DropDown"></div>
+                </div>
             </div>
         )
     }
 }
 
 export default NavBar;
+{/* <Link className = "Link_Properties" to = "/news">
+    <p className = "Link_Text"> News </p>
+</Link>
+<Link className = "Link_Properties" to = "scores">
+    <p className = "Link_Text"> Scores </p>
+</Link>
+<Link className = "Link_Properties" to = "/schedule">
+    <p className = "Link_Text"> Schedule </p>
+</Link>
+<Link className = "Link_Properties" to = "/roster">
+    <p className = "Link_Text"> Roster </p>
+</Link> */}
